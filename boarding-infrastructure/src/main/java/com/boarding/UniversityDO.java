c8d7ff655610126408cf22821037d10de3305f1e
lingyue <xujinwei@youzan.com>
2021-06-06 17:47:36 +0800

    init

---
+++ boarding-infrastructure/src/main/java/com/boarding/UniversityDO.java
@@ -0,0 +1,52 @@
+package com.boarding;
+
+import java.util.Date;
+
+/**
+ * @Author ling yue
+ * @Date 2021/6/6 下午5:42
+ * @Pkg com.boarding
+ * @Desc description
+ */
+public class UniversityDO {
+
+    private Long id;
+
+    private Long universityId;
+
+    private String universityName;
+
+    private Boolean hasPostgraduateInstitute;
+
+    private String subordination;
+
+    private Boolean decidePassingScoreBySelf;
+
+    private String questionSite;
+
+    private String notationSite;
+
+    private String adjustmentSite;
+
+    private String recruitmentSite;
+
+    private String locationCode;
+
+    private String locationName;
+
+    private String subLocationCode;
+
+    private String subLocationName;
+
+    private Boolean projectNef;
+
+    private Boolean projectToo;
+
+    private Boolean projectFirstClassUniversity;
+
+    private Boolean projectFirstClassSubject;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+}
