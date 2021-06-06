ab4da630758057b2b98b9eaa865c37b7e1b63f08
lingyue <xujinwei@youzan.com>
2021-06-06 17:30:12 +0800

    init

---
+++ boarding-web/src/main/java/com/boarding/boardingcore/BoardingCoreApplication.java
@@ -0,0 +1,12 @@
+package com.boarding.boardingcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardingCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardingCoreApplication.class, args);
+    }
+}
---
+++ src/test/java/com/boarding/boardingcore/BoardingCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boarding.boardingcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardingCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
