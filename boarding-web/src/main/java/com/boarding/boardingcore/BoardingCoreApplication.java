8b4f87a0371d831ad5ef42ed6833fefb14f76b3d
lingyue <xujinwei@youzan.com>
2021-06-06 17:39:24 +0800

    mysql 接入

--- boarding-web/src/main/java/com/boarding/boardingcore/BoardingCoreApplication.java
+++ boarding-web/src/main/java/com/boarding/boardingcore/BoardingCoreApplication.java
@@ -2,6 +2,7 @@ package com.boarding.boardingcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class BoardingCoreApplication {
