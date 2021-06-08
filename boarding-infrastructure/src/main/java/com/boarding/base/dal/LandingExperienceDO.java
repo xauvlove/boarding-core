626dc4062d945f2af71c3c1e5debd76a0d235d61
lingyue <xujinwei@youzan.com>
2021-06-08 12:44:49 +0800

    xml mybatis

--- boarding-infrastructure/src/main/java/com/boarding/base/dal/LandingExperienceDO.java
+++ boarding-infrastructure/src/main/java/com/boarding/base/dal/LandingExperienceDO.java
@@ -1,5 +1,6 @@
 package com.boarding.base.dal;
 
+import com.boarding.base.enums.MasterTypeEnum;
 import lombok.Data;
 
 import java.util.Date;
@@ -14,107 +15,108 @@ import java.util.Date;
 public class LandingExperienceDO {
 
     /**
-     * -
+     * - id
      */
     private Long id;
 
     /**
-     * -
+     * - universityId
      */
     private Long universityId;
 
     /**
-     * -
+     * - universityName
      */
     private String universityName;
 
     /**
      * -
+     * @see MasterTypeEnum
      */
     private Integer masterType;
 
     /**
-     * -
+     * - 门类
      */
     private String categoryCode;
 
     /**
-     * -
+     * - 一级学科编号
      */
     private String firstSubjectCode;
 
     /**
-     * -
+     * - 二级学科编号
      */
     private String secondarySubjectCode;
 
     /**
-     * -
+     * - 专业
      */
     private String discipline;
 
     /**
-     * -
+     * - 研究方向
      */
     private String direction;
 
     /**
-     * -
+     * - 考研届 如 2021
      */
     private Integer examYear;
 
     /**
-     * -
+     * - 初试分数 * 100
      */
     private Long examScore;
 
     /**
-     * -
+     * - 初试排名
      */
     private Integer examRank;
 
     /**
-     * -
+     * - 复试时间 ：20210322
      */
     private Integer reExamDate;
 
     /**
-     * -
+     * - 复试分数
      */
     private Integer reExamScore;
 
     /**
-     * -
+     * - 总排名 初试+复试 加权排名
      */
     private Integer rank;
 
     /**
-     * -
+     * - 总排名 初试+复试 加权总分数
      */
     private Long weightTotalScore;
 
     /**
-     * -
+     * - 联系方式
      */
     private String contactInfo;
 
     /**
-     * -
+     * - 本科学校名称
      */
     private String bachelorUniversityName;
 
     /**
-     * -
+     * - 经验分享
      */
     private String experience;
 
     /**
-     * -
+     * - createdAt
      */
     private Date createdAt;
 
     /**
-     * -
+     * - updatedAt
      */
     private Date updatedAt;
 }
